fbdad368de89090a072c8cd053cced7445a162a5
Techcable <Techcable@outlook.com>
2014-11-23 01:45:53 -0700

    Start on CmdTaxFaction Add A Whole Bunch of Format Code

---
+++ src/main/java/com/massivecraft/factions/tax/FactionsTax.java
@@ -0,0 +1,34 @@
+package com.massivecraft.factions.tax;
+
+import org.bukkit.Bukkit;
+
+import com.massivecraft.factions.Conf;
+import com.massivecraft.factions.P;
+
+public class FactionsTax {
+	private boolean gracePeriod;
+	
+	public boolean isGracePeriod() {
+		return gracePeriod;
+	}
+	public void setGracePeriod(boolean gracePeriod) {
+		this.gracePeriod = gracePeriod;
+	}
+	public void enable() {
+		if (!Conf.taxEnabled) return;
+		if (Conf.taxFirstStartedMill == 0)  { //Haven't started taxes before
+			Conf.taxFirstStartedMill = System.currentTimeMillis();
+			setGracePeriod(true);
+		} else if (System.currentTimeMillis() - Conf.taxFirstStartedMill > Conf.taxFirstStartedGraceMill) {
+			setGracePeriod(true);
+		} else {
+			setGracePeriod(false);
+		}
+		
+	}
+	
+	private FactionsTaxPlayerListener playerListener = new FactionsTaxPlayerListener(this);
+	public void registerListeners() {
+		Bukkit.getPluginManager().registerEvents(playerListener, P.p);
+	}
+}
